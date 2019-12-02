94574a2f6b02c9fe8b67c3e4f542bf4afa49032b
LilasAll <lilas.allard@gmail.com>
2019-12-02 12:34:10 +0100

    Adding content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-    root 'static#index'
+  root 'static#index'
+
+  get '/', to: 'static#index'
+  get '/contact', to: 'static#contact'
+
+  resources :user
+  resources :event
+  resources :article
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
