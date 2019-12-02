42a5f7beec9eb57907aa54ce51e2165b3d31f52e
LilasAll <lilas.allard@gmail.com>
2019-12-02 11:39:10 +0100

    Adding validations for each table

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
 	belongs_to :user
 
 	## Validations : 
-	validates :title, presence: true 
-	validates :content, presence: true, length: { in: 20..600, message: "Le titre doit contenir entre 20 et 600 caractères."}
+	validates :title, presence: true, length: { in: 10..100, message: "Le titre doit contenir entre 10 et 100 caractères."}
+	validates :content, presence: true, length: { in: 20..10000, message: "Le contenu de l'article doit contenir entre 20 et 10000 caractères."}
 
 end
