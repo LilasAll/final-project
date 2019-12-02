e26f6824280a0b8e6fba871fd8aa03a97756738d
thezo23 <blintheo23@gmail.com>
2019-12-02 10:11:02 +0100

    created tables

--- Gemfile
+++ Gemfile
@@ -3,6 +3,37 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+# GEM INSTALLED#==============================================
+gem 'htmlbeautifier'
+
+gem "aws-sdk-s3", require: false
+
+gem 'binding_of_caller'
+
+gem 'letter_opener'
+
+gem 'better_errors'
+
+gem 'stripe'
+
+gem 'devise'
+
+gem 'faker'
+
+gem 'dotenv'
+
+gem 'dotenv-rails'
+
+gem 'table_print'
+
+gem 'bcrypt'
+
+gem 'mailjet'
+
+gem 'rubocop'
+
+# GEM INSTALLED#==============================================
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4'
 # Use postgresql as the database for Active Record
---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_12_02_090503) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.integer "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "start_date"
+    t.integer "duration"
+    t.string "title"
+    t.text "description"
+    t.string "location"
+    t.bigint "admin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_events_on_admin_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "pseudo"
+    t.string "city"
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "is_admin"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+end
