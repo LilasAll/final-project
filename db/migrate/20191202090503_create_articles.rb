574302591bc8a82bb538167035dbdcd025990c4d
LilasAll <lilas.allard@gmail.com>
2019-12-02 10:54:03 +0100

    Changing Article migration

--- db/migrate/20191202090503_create_articles.rb
+++ db/migrate/20191202090503_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
   	create_table :articles do |t|
 
-  	 	t.integer :title
+  	 	t.string :title
   	 	t.text :content
 
   	 	t.belongs_to :user, index: true
