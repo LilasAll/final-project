ee797b2dc449df6b1602e8c282d0cf1d8b5053c3
thezo23 <blintheo23@gmail.com>
2019-12-04 11:48:37 +0100

    fix profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params[:id].to_i == current_user.id.to_i
+      @user = User.find(params[:id])
+
       @current_user = current_user
-    else
-      redirect_to '/'
-    end
+  
     # on définit les évenements créés
     @events = Event.all
 
