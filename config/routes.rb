e56e10dba7239bda51afc3d862dc2f1e2416a156
thezo23 <blintheo23@gmail.com>
2019-12-05 16:02:10 +0100

    comments done!

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@
 Rails.application.routes.draw do
   root 'static#index'
 
+
   devise_for :users
 
   resources :events do
     resources :avatars, only: [:create]
     resources :attendances
+    
 
     # Important pour l'admin !
     collection do
@@ -30,6 +32,7 @@ Rails.application.routes.draw do
   end
 
   resources :admins
+  resources :comments, except: [:new, :index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_094454) do
+ActiveRecord::Schema.define(version: 2019_12_05_140931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,16 @@ ActiveRecord::Schema.define(version: 2019_12_05_094454) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_attendances_on_event_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "events", force: :cascade do |t|
