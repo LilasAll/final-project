71c9f18c494bb9bff53070976b7aa5c9582e8d5e
LilasAll <lilas.allard@gmail.com>
2019-12-09 17:21:42 +0100

    adding bug spec

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Bug < ApplicationRecord
+
+	validates :content, presence: :true
+
 end
--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -3,5 +3,52 @@
 require 'rails_helper'
 
 RSpec.describe Bug, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @bug = FactoryBot.create(:bug)
+  end
+
+  it 'has a valid factory' do
+    expect(build(:bug)).to be_valid
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(@bug).to be_a(Bug)
+    end
+
+    describe '#content' do
+      it { expect(@bug).to validate_presence_of(:content) }
+    end
+
+    describe '#user_name' do
+      it { expect(@bug).to validate_presence_of(:user_name) }
+    end
+
+  end
+
+  context 'associations' do
+    describe 'some association' do
+      #
+    end
+  end
+
+  context 'callbacks' do
+    describe 'some callbacks' do
+      # teste ce callback
+    end
+  end
+
+  context 'public instance methods' do
+    describe '#content' do
+      it 'should return a string' do
+        expect(@bug.content).to be_a(String)
+      end
+    end
+  end
+
+  context 'public class methods' do
+    describe 'self.some_method' do
+      # teste cette méthode
+    end
+  end
 end
