50a22b7b3f266bb815803995d38cf20e2d098697
thezo23 <blintheo23@gmail.com>
2019-12-09 19:14:28 +0100

    some issues with messages

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2019_12_09_131347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,9 +64,7 @@ ActiveRecord::Schema.define(version: 2019_12_09_131347) do
     t.string "content"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_bugs_on_user_id"
-
   end
-
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -153,8 +150,6 @@ ActiveRecord::Schema.define(version: 2019_12_09_131347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
