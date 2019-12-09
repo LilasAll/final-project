c954b0ba310850a3d510421b1f76eb2a48354b06
thezo23 <blintheo23@gmail.com>
2019-12-09 21:40:28 +0100

    desactivated link for heroku

--- app/assets/javascripts/main_map.js
+++ app/assets/javascripts/main_map.js
@@ -1,25 +1,25 @@
 //let overlayTileLayer = "https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
 
-let initialZoom = 12;
+//let initialZoom = 12;
 
 // [lat,long]
-let coordLille= [50.632971, 3.058585];
+//let coordLille= [50.632971, 3.058585];
 
 // main_map se refere à l'id de la div dans laquelle la map est contenue
-let mymap = L.map('main_map').setView(coordLille, initialZoom);
+//let mymap = L.map('main_map').setView(coordLille, initialZoom);
 
-L.tileLayer(overlayTileLayer,{maxZoom :100}).addTo(mymap);
+//L.tileLayer(overlayTileLayer,{maxZoom :100}).addTo(mymap);
 
 // on définit les pins : 
-console.log(gon.cities_secondhand);
+//console.log(gon.cities_secondhand);
 
-let cities = gon.cities;
+//let cities = gon.cities;
 
-let cities_secondhand_array = gon.cities_secondhand
+//let cities_secondhand_array = gon.cities_secondhand
 
-cities_secondhand_array.addTo(mymap)
+//cities_secondhand_array.addTo(mymap)
 
-cities_secondhand_array.forEach(function(secondhand){
-   let marker = L.marker([place.latitude ,place.longitude]);
-   marker.addTo(mymap);
-});
+//cities_secondhand_array.forEach(function(secondhand){
+  // let marker = L.marker([place.latitude ,place.longitude]);
+   //marker.addTo(mymap);
+//});
