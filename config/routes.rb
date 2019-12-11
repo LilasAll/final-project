b43896e8bc9bc708947a62865b035f8057d54d8c
Mahaut <mahautdeguilhem@gmail.com>
2019-12-11 18:03:51 +0100

    tag show ok

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,7 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
+
+  resources :tags, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
