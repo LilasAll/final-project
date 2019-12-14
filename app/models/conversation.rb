26a44b017b0a2800bc584f1552a4444f3ace4866
thezo23 <blintheo23@gmail.com>
2019-12-14 11:18:04 +0100

    team fixed

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,6 +3,7 @@
 class Conversation < ActiveRecord::Base
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+  acts_as_readable on: :created_at
 
   has_many :messages, dependent: :destroy
   validates_uniqueness_of :sender_id, scope: :recipient_id
