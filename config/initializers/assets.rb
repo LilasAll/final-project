ecfac7bed37ba4527594460d21d5f3a1cd1323d3
Mahaut <mahautdeguilhem@gmail.com>
2019-12-16 00:44:42 +0100

    prod photos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,6 +17,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'javascripts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
 
